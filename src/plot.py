ecaac7b328f6277af7595de16e938e08668bbf2f
Oline <oline.a.ranum@gmail.com>
2020-06-30 15:29:40 +0200

    Made general variables for plotting module

--- src/plot.py
+++ src/plot.py
@@ -1,46 +1,52 @@
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
+import pandas as pd
 
 class plot():
 
-    def __init__(self, data, M2D, M):
+    def __init__(self, data, ActivationMatrix, CoordinateMatrix):
         self.data = data
-        self.M2D = M2D
-        self.M = M
+        self.ActivationMatrix = ActivationMatrix
+        self.CoordinateMatrix = CoordinateMatrix
         self.line = []
 
+        self.fsize = 20                     # Fontsize
+        self.psize = 25                     # Padding size
+        self.tpsize = 30                    # Titlepaddingsize
 
-    def plot_FACT(self):      
-        plt.scatter(self.data['z'], self.data['r'])
-        plt.plot
-        plt.show()
+        self.geometry = pd.read_csv('settings/settings.csv', header = 0)
+        self.frames_ = int(self.geometry['frame'][0])
         
+        self.detector_xrange = (-120, 120)
+        self.detector_yrange =(68, 100)
 
 
-    def init(self):
-        r = self.M[:,1]*self.M2D[:,0]
-        z = self.M[:,2]*self.M2D[:,0]
+    def initAnimation(self):
+        r = self.CoordinateMatrix[:,1]*self.ActivationMatrix[:,0]
+        z = self.CoordinateMatrix[:,2]*self.ActivationMatrix[:,0]
         self.line.set_data(z,r)
         return self.line
 
     def Animate(self, i):
-        r = self.M[:,1]*self.M2D[:,i]
-        z = self.M[:,2]*self.M2D[:,i]
+        r = self.CoordinateMatrix[:,1]*self.ActivationMatrix[:,i]
+        z = self.CoordinateMatrix[:,2]*self.ActivationMatrix[:,i]
         self.line.set_data(z,r)
         return self.line
 
+
+
     def plot_FACT_live(self):            
-        fig = plt.figure(figsize = (20,10))
-        ax = plt.axes(xlim=(-120, 120), ylim=(68, 100))
-        ax.set_xlabel('z [mm]', fontsize = 20,  labelpad = 25)
-        ax.set_ylabel('r [mm]', fontsize = 20, labelpad = 25)
-        ax.tick_params(axis='both', which='major', labelsize=20)
-        ax.tick_params(axis='both', which='minor', labelsize=20)
-        ax.set_title('FACT Detector', fontsize = 25, pad = 30)
+        fig = plt.figure(figsize = (self.fsize,10))
+        ax = plt.axes(xlim=self.detector_xrange, ylim = self.detector_yrange)
+        ax.set_xlabel('z [mm]', fontsize = self.fsize,  labelpad =self.psize)
+        ax.set_ylabel('r [mm]', fontsize = self.fsize, labelpad =self.psize)
+        ax.tick_params(axis='both', which='major', labelsize=self.fsize)
+        ax.tick_params(axis='both', which='minor', labelsize=self.fsize)
+        ax.set_title('FACT Detector', fontsize =self.psize, pad = self.tpsize)
         self.line, = ax.plot([], [], '-ro', lw = 2)
-        r = self.M[:,1]*self.M2D[:,0]
-        z = self.M[:,2]*self.M2D[:,0]
+        r = self.CoordinateMatrix[:,1]*self.ActivationMatrix[:,0]
+        z = self.CoordinateMatrix[:,2]*self.ActivationMatrix[:,0]
         self.line.set_data(z,r)
-        anim = animation.FuncAnimation(fig, self.Animate, init_func=self.init,
-                               frames=500, interval=20)
+        anim = animation.FuncAnimation(fig, self.Animate, init_func=self.initAnimation,\
+            frames=self.frames_, interval=self.fsize)
         plt.show()
