e57d8a748dd7f600e2fd9e35324b5608fa964bc6
Oline <oline.a.ranum@gmail.com>
2020-07-07 10:48:47 +0200

    Added module for scattering plot

--- src/plot.py
+++ src/plot.py
@@ -5,11 +5,10 @@ import seaborn as sns
 
 class plot():
 
-    def __init__(self, data, param, ActivationMatrix, CoordinateMatrix):
+    def __init__(self, data, param, build):
         self.data = data
         self.param = param
-        self.ActivationMatrix = ActivationMatrix
-        self.CoordinateMatrix = CoordinateMatrix
+        self.build = build
         self.line = []
 
         self.fsize = 20                     # Fontsize
@@ -19,16 +18,29 @@ class plot():
         self.detector_xrange = (-120, 120)
         self.detector_yrange =(68, 100)
 
+    def scatter(self, x, y, time):
+        fig = plt.figure(figsize = (self.fsize,10))
+        sns.set_style("darkgrid")
+        ax = plt.axes(xlim=self.detector_xrange, ylim = (69.9, 71))
+        ax.set_xlabel('z [mm]', fontsize = self.fsize,  labelpad =self.psize)
+        ax.set_ylabel('r [mm]', fontsize = self.fsize, labelpad =self.psize)
+        ax.tick_params(axis='both', which='major', labelsize=self.fsize)
+        ax.tick_params(axis='both', which='minor', labelsize=self.fsize)
+        ax.set_title('FACT Detector', fontsize =self.psize, pad = self.tpsize)
+        plt.scatter(x,y,c = time, cmap='Reds', s = 40)
+        plt.colorbar()
+        plt.show()
+        pass
 
     def initAnimation(self):
-        r = self.CoordinateMatrix[:,1]*self.ActivationMatrix[:,0]
-        z = self.CoordinateMatrix[:,2]*self.ActivationMatrix[:,0]
+        r = self.param.CoordinateMatrix[:,1]*self.build.ActivationMatrix[:,0]
+        z = self.param.CoordinateMatrix[:,2]*self.build.ActivationMatrix[:,0]
         self.line.set_data(z,r)
         return self.line
 
     def Animate(self, i):
-        r = self.CoordinateMatrix[:,1]*self.ActivationMatrix[:,i]
-        z = self.CoordinateMatrix[:,2]*self.ActivationMatrix[:,i]
+        r = self.param.CoordinateMatrix[:,1]*self.build.ActivationMatrix[:,i]
+        z = self.param.CoordinateMatrix[:,2]*self.build.ActivationMatrix[:,i]
         self.line.set_data(z,r)
         return self.line
 
@@ -42,8 +54,8 @@ class plot():
         ax.tick_params(axis='both', which='minor', labelsize=self.fsize)
         ax.set_title('FACT Detector', fontsize =self.psize, pad = self.tpsize)
         self.line, = ax.plot([], [], '-ro', lw = 2)
-        r = self.CoordinateMatrix[:,1]*self.ActivationMatrix[:,0]
-        z = self.CoordinateMatrix[:,2]*self.ActivationMatrix[:,0]
+        r = self.param.CoordinateMatrix[:,1]*self.build.ActivationMatrix[:,0]
+        z = self.param.CoordinateMatrix[:,2]*self.build.ActivationMatrix[:,0]
         self.line.set_data(z,r)
         anim = animation.FuncAnimation(fig, self.Animate, init_func=self.initAnimation,frames=self.param.frames, interval=self.fsize)
         plt.show()
