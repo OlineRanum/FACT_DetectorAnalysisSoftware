fba8923a056fb0141381b88b02c04256e6bbc3b9
Oline <oline.a.ranum@gmail.com>
2020-07-07 10:47:05 +0200

    Rework for unittesting avilabilities

--- src/Setup.py
+++ src/Setup.py
@@ -39,23 +39,23 @@ class Setup():
     
 
     def Initiate(self):
-        # Run SetUp Modules
-        self.FindRisingEdge()
+        # The module called from main, that run the BASIC SetUp Modules
+        self.FindRisingEdge(self.data, self.param.t_res, self.param.rising_edge, self.param.edge_buffer)
         self.CropData()
-        self.ConstructActivationMatrix()
+        self.ConstructActivationMatrix(self.data, self.param.N_fibers, self.time, self.param.t_res)
         self.ResetTime()
 
-        return self.data, self.ActivationMatrix
+        return self.data
 
-    def FindRisingEdge(self):
-        t   = np.array(self.data['t'], dtype = int)
-        tot = np.array(self.data['tot'],  dtype = int)
+    def FindRisingEdge(self, df, t_resolution, edge_lim, edge_buffer):
+        t   = np.array(df['t'], dtype = int)
+        tot = np.array(df['tot'],  dtype = int)
 
-        for i in range(len(self.data['N'])):
-            self.count[t[i]//self.param.t_res: (t[i]+ tot[i])//self.param.t_res] += 1
+        for i in range(len(df['N'])):
+            self.count[t[i]//t_resolution: (t[i]+ tot[i])//t_resolution] = 1
         
         # Find index where count > param.rising_edge and give buffer = param.edge_buffer
-        self.EdgeIndex = int(np.argmax(self.count >= self.param.rising_edge)-self.param.edge_buffer)
+        self.EdgeIndex = int(np.argmax(self.count >= edge_lim)-edge_buffer)
 
     def CropData(self):
         EndIndex = self.EdgeIndex+int(self.param.frames)
@@ -69,18 +69,21 @@ class Setup():
         self.data = self.data[(self.data['t'] >= tmin) & (self.data['t'] <= tmax)].reset_index(drop = True)
 
 
-    def ConstructActivationMatrix(self):
-        self.ActivationMatrix = np.empty((self.param.N_fibers,len(self.time)))
+    def ConstructActivationMatrix(self, df, N_fibers, time_frame, t_resolution):
+        self.ActivationMatrix = np.empty((N_fibers, len(time_frame)))
         self.ActivationMatrix[:] = np.NaN
     
-        N = np.array(self.data['N'], dtype = int)
-        t = np.array(self.data['t'], dtype = int)
-        tot = np.array(self.data['tot'],  dtype = int)
-
+        N = np.array(df['N'], dtype = int)
+        t = np.array(df['t'], dtype = int)
+        tot = np.array(df['tot'],  dtype = int)
+        t0_ = int(time_frame[0])
+        print(t0_)
+        
         # Count all active fibers
         for i in range(len(N)):
-            self.ActivationMatrix[N[i], (t[i]-self.t0)//self.param.t_res:(t[i]-self.t0+tot[i])//self.param.t_res] = 1
+            self.ActivationMatrix[N[i], (t[i]-t0_)//t_resolution:(t[i]-self.t0+tot[i])//t_resolution] = 1
 
+        return self.ActivationMatrix
 
     def ResetTime(self):
         self.time = self.time - self.time[0]
