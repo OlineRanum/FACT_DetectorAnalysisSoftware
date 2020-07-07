c81e05e161f73c749e2c0bea146000b6cf074278
Oline <oline.a.ranum@gmail.com>
2020-07-07 10:48:08 +0200

    Added UnitTesting instance for main file

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from AnalysisToolBox import AnalysisToolBox
 from plot import plot
 from LoadData import LoadData
 from FileConverter import FileConverter
-
+from UnitTesting import UnitTesting
 import time
 
 start = time.time()
@@ -24,15 +24,20 @@ param.Initiate()
 RF = ReadFiles(PathData, folder)
 data = RF.GetCSV()
 
+UT = UnitTesting()
+UT.SetupTestingEnvironment()
+
+
+"""
 build = Setup(data, param)
-data, ActivationMatrix= build.Initiate() 
+data  = build.Initiate() 
 
-#ATB = AnalysisToolBox(data, param, ActivationMatrix, CoordinateMatrix)
-#ATB.Initiate_Standard_Analysis()
+ATB = AnalysisToolBox(data, param, build)
+ATB.Initiate_Standard_Analysis()
 
-#P = plot(data, param, ActivationMatrix, param.CoordinateMatrix)
+#P = plot(data, param, build)
 #P.plot_FACT_live()
-
+"""
 end = time.time()
 print('Full Run: %.5f' % (end - start), 's')
 
