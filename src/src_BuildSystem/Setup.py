83337b8e24ee5ca26d98e8b187e6b83b0e99f8aa
Oline <oline.a.ranum@gmail.com>
2020-07-16 14:24:56 +0200

    Major reconstruction to SetUp class to better structure

--- src/src_BuildSystem/Setup.py
+++ src/src_BuildSystem/Setup.py
@@ -24,15 +24,13 @@ class Setup():
         self.param = param
         self.MainData = MainData
 
-        # Timebase from min and max time in main dataframe, with intervals corresponding to the detectors temporal resolution
-        self.time = np.arange(self.MainData['t'].loc[np.argmin(self.MainData['t'])],self.MainData['t'].loc[np.argmax(self.MainData['t'])] + self.param.t_res, self.param.t_res)
-        # Array to be filled with the number of activated fibers at eatch time step
-        self.count = np.zeros(len(self.time))
-
+        # set natives
+        self.time = np.array(0)
+        self.count = np.array(0)
         # The minimum time of the main dataframe
         self.t0 = 0
         # The Index of which the number of activated fibers is above param.edge
-        self.EdgeIndex = 0
+        self.EdgeIndex = 0 
         # 2D array to be filled by N fibers x time binary values, one indicating that the fiber is active and zero that it is inactive
         # Each row in the array corresponds to a spesific fiber
         self.ActivationMatrix = np.empty((0,0))  
@@ -48,74 +46,59 @@ class Setup():
             Processed dataframe  
         """
 
-        self.FindRisingEdge(self.MainData, self.param.t_res, self.param.rising_edge)
-        if self.EvaluateFile() is None:
-            return None
+        # Crop and clean data
+        self.PrepareFactData(self.MainData, self.param.t_res, self.param.rising_edge)
 
-        self.CropData()
+        # Build Activation matrix
         self.ConstructActivationMatrix(self.MainData, self.param.N_fibers, self.time, self.param.t_res)
-        self.ResetTime()
-        self.CombineDatabases()
-
-        return self.MainData
-
-    def CombineDatabases(self):
-        CoordinateFrame = pd.DataFrame(self.param.FiberMapper, columns = ['N', 'r', 'z'])
-        CoordinateFrame['N'] = CoordinateFrame['N'].astype(int)
-        self.MainData = pd.merge(self.MainData, CoordinateFrame, on =['N'])
         
-    def EvaluateFile(self):
-        if np.max(self.count) < 100:
-            print('Particles never arrived')
-            return None
-        else:
-            return 'Ok File'
+        self.time = self.time - self.time[0]
+        self.MainData['t'] = self.MainData['t']-self.MainData['t'].iloc[0]  
 
 
-    def FindRisingEdge(self, df, t_resolution, edge_lim):
-        """ Functionality:
-        Finds the rising edge of activated fibers, indicating the arrival of positronium 
-        where the point where the number of active fibers > edge_lim
-        
-        Parameters:                      
-            df:                 MainDataframe
-            t_resolution:       temporal resolution of detector
-            edge_lim:           number of activated fibers to determine incoming particle burst/rising edge
+        return self.MainData, self.time, self.count
 
-        Return:
-            EdgeIndex:          Index where rising edge (N_fibers activated > treshold edge_lim) is found
-            Count:              Array containing the number of activated fibers at each time t
-        """
-
+    def PrepareFactData(self, df, t_resolution, edge_lim):
         t   = np.array(df['t'], dtype = int)//t_resolution
         tot = np.array(df['tot'],  dtype = int)//t_resolution
+
+        # Timebase from min and max time in main dataframe, with intervals corresponding to the detectors temporal resolution
+        self.time = np.arange(df['t'].loc[np.argmin(df['t'])],df['t'].loc[np.argmax(df['t'])] + self.param.t_res, self.param.t_res)
+        # Array to be filled with the number of activated fibers at eatch time step
+        self.count = np.zeros(len(self.time))
+
         
         # Count number of activated fibers at eatch time t, given from main dataframe
         for i in range(len(df)):
             self.count[t[i]: t[i]+ tot[i]] += 1
 
+        # Test that file is in fact a runfile and not a claibration file
+        if self.EvaluateFile() is None:
+            return None
+        
+
         # Find index where count > param.rising_edge
-        self.EdgeIndex = int(np.argmax(self.count >= edge_lim))
+        self.EdgeIndex = int(np.argmax(self.count >= edge_lim)) -50
+        EndIndex = self.EdgeIndex+int(self.param.frames) 
 
-        return self.EdgeIndex, self.count
 
-    def CropData(self):
-        """ Functionality:
-        Crop data to only contain data where t > t(rising_edge) to t < t(N_frames)
-        
-        Returns:
-            Cropped main dataframe, timebase and count
-        """
-        
-        EndIndex = self.EdgeIndex+int(self.param.frames)
-        
+        # Crop Data to zones restricted by EdgeIndex and EndIndex
         self.MainData = self.MainData[(self.MainData['t'] >= self.time[self.EdgeIndex]) &\
             (self.MainData['t'] <= self.time[EndIndex])].reset_index(drop = True)
+
         self.time = self.time[self.EdgeIndex: EndIndex]
         self.count = self.count[self.EdgeIndex: EndIndex]
 
         self.t0 = int(self.time[0])
 
+        # Reset Time
+
+        # Make Combined Database        
+        CoordinateFrame = pd.DataFrame(self.param.FiberMapper, columns = ['N', 'r', 'z'])
+        CoordinateFrame['N'] = CoordinateFrame['N'].astype(int)
+        self.MainData = pd.merge(self.MainData, CoordinateFrame, on =['N'])   
+
+        
     def ConstructActivationMatrix(self, df, N_fibers, time_frame, t_resolution):
         """  Functionallity: 
         Build binary 2D N_fibers x Time Activation matrix for animation of activated fibers
@@ -138,22 +121,17 @@ class Setup():
         tot = np.array(df['tot'],  dtype = int)//t_resolution
         t0_ = self.t0//t_resolution
 
+        print(t[0])
+
         # Count all active fibers
         for i in range(len(N)):
             self.ActivationMatrix[N[i], t[i]-t0_:t[i]-t0_+tot[i]] = 1
 
-        return self.ActivationMatrix
+        return self.ActivationMatrix   
 
-    def ResetTime(self):
-        """ Functionality: 
-        Resets the time of the time-axis and MainData to zero. 
-        
-        Returns:
-            Data of relevant timeframe 
-        """
-        self.time = self.time - self.time[0]
-        self.MainData['t'] = self.MainData['t']-self.MainData['t'].iloc[0]                  
-
-
-
-
+    def EvaluateFile(self):
+        if np.max(self.count) < 100:
+            print('FileTypeError: This file is a calibration file, positronium never arrived')
+            return None
+        else:
+            return 'Ok File'
